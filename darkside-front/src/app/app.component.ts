a19a176e34376e6e3c34dbc4365b913c1eb483d8
Dylan Deleplanque <dylan.deleplanque@davidson.fr>
2018-04-04 14:05:07 +0200

    Modication de la partie about pour le mobile

--- darkside-front/src/app/app.component.ts
+++ darkside-front/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { WOW } from 'wowjs/dist/wow.min';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    const wow = new WOW(
+      {
+        boxClass:     'wow',
+        animateClass: 'animated',
+        offset:       0,
+        mobile:       true,
+        live:         false,
+        callback:     function(box) {
+        },
+        scrollContainer: null
+      }
+    );
+    wow.init();
+  }
+
 }
