2f271737f7d98b0149dfa8706e7d810d9b6835e8
deleplanque <d.dylan62138@gmail.com>
2018-04-15 19:54:52 +0200

    Ajout du routing

--- darkside-front/src/app/app.routing.module.ts
+++ darkside-front/src/app/app.routing.module.ts
@@ -3,9 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {AppComponent} from './app.component';
 
 const routes: Routes = [
-
-  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
-  { path: '/accueil', component: AppComponent }
+  
 ];
 
 @NgModule({
